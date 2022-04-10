8ab17abc99c5062ea563501b7cd1ffb05bc4bd77
Rhodamine-Han <lovkis8@naver.com>
2022-04-10 21:52:43 +0900

    making ref.data for TestProject

--- TestProject.py
+++ TestProject.py
@@ -2,32 +2,33 @@ import pandas as pd
 
 df = pd.read_csv('OS_test_sorter.csv')
 
-# countFORGRP = 1
-count = 0
 
+# #already made how to do numbering
 
-# MyLen = len(df['Unnamed: 3'])
+# count = 0
+
+# Numofsam = int(input('number of data? : '))
+
+# df['Unnamed: 4'] = 0
 
 # while True:
-#     if count < MyLen:
-#         df['Unnamed: 4'] = countFORGRP
+#     if count < Numofsam:
+#         df['Unnamed: 4'][count*6:count*6+4] = count+1
+#         count += 1
 #     else:
-
-NumofRef = int(input('number of ref.? : '))
-
-df['Unnamed: 4'] = 0
-
-# df['Unnamed: 4'][0:4] = 1
-
-while True:
-    if count < NumofRef:
-        df['Unnamed: 4'][count*4:count*4+4] = count+1
-        df['Unnamed: 4'][count*4+4:count*4+6] = 0
-        count += 1
-
-    else:
-        break
+#         break
 
 
+Numofsam = int(input('How many samples? : '))
+Numofref = int(input('How many references? : '))
 
-print(df)
+df_ref_raw = df.loc[0:((6*Numofref)-2)]
+
+df_ref_D0N = df_ref_raw.groupby('OS1').get_group('D0N')
+df_ref_D0P = df_ref_raw.groupby('OS1').get_group('D0P')
+df_ref_D1N = df_ref_raw.groupby('OS1').get_group('D1N')
+df_ref_D1P = df_ref_raw.groupby('OS1').get_group('D1P')
+
+print(df_ref_D0N)
+
+
