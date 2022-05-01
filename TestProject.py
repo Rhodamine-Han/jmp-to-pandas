65dafa2799b008163ba6f1d11e3ec83056a9de30
Rhodamine-Han <lovkis8@naver.com>
2022-05-01 16:35:21 +0900

    committing for after-processing on TestProject

--- TestProject.py
+++ TestProject.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+pd.options.display.float_format = '{:.3f}'.format
+
 df = pd.read_csv('OS_test_sorter.csv')
 
 
@@ -18,7 +20,13 @@ for i in range(0, Numofsam - Numofref):
     df_data_result.iloc[6*i+1:6*i+5, 0] = df_ref_mean.iloc[0:4,0]
     df_data_result.iloc[6*i+1:6*i+5, 2] = df_ref_mean.iloc[0:4,1]
 
+# df_data_result.drop(df_data_result.columns['OS1'], inplace=True)
+
+df_data_result.columns = ['Ref_Forward', 'data_Forward', 'Ref_Reverse', 'data_Reverse']
+
+print('Result\n\n')
 print(df_data_result)
+print('\n\ncomplete')
 
 
 df_data_result.to_csv('output.csv')
